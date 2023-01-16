ce206dfb3bd616852c8919640f85bde07d32f10f
Oliver Akatugba <oliver.akatugba@gmail.com>
2023-01-16 16:19:20 +0100

    .

--- wordle/faster_wordle.py
+++ wordle/faster_wordle.py
@@ -1,15 +1,17 @@
 
 # Read in all the words
-words = [i for i in open('wordlist_2.txt', 'r').read().split('\n')]
+words = [i for i in open('wordlist.txt', 'r').read().split('\n')]
 
 print(f"Total words: {len(words)}")
 
 # Filter out words with duplicate characters
-words_bitmap = {
-}
+words_bitmap = {}
 processed_words = set()
+word_sets = [set()]*26
 
 for word in words:
+    if len(word) != 5:
+        continue
     bitmap = 0
 
     for char in word:
@@ -24,38 +26,54 @@ for word in words:
 
 print(f"Filtered words: {len(processed_words)}")
 
+for word in processed_words:
+    for bit in range(26):
+        if not word & (1 << bit):
+            word_sets[bit].add(word)
+
+
 five_word_combinations = []
 checked_combs = set()
-# 24006 3 word combs
-for first_word in {10562}:
+
+def conv_word(word):
+    return '|'.join(words_bitmap[word])
+
+def get_candidates(words):
+    candidates = processed_words.copy()
+
+    for bit in range(26):
+        if words & (1 << bit):
+            candidates -= word_sets[bit]
+
+    return candidates
+
+for ind, first_word in enumerate(processed_words):
     combinations = [[[first_word], first_word]]
 
     for tot_words in range(4):
-        print(f"Finding all combinations of {tot_words+2} words with starting word \"{first_word}\"")
+        print(f"Finding all combinations of {tot_words+2} words with starting word \"{conv_word(first_word)}\" (index {ind})")
 
         new_combs = []
-        for index, comb in enumerate(combinations):
-            if index % 100 == 0:
-                print(index)
+        for comb in combinations:
             chosen_words, letters = comb
-            candidate_words = [word for word in processed_words if (letters | word).bit_count() == (tot_words+2)*5]
+            candidate_words = get_candidates(letters)
             for next_word in candidate_words:
                 unique_letters = letters | next_word
 
-                if unique_letters.bit_count() == (tot_words+2)*5 and unique_letters not in checked_combs:
+                if unique_letters not in checked_combs:
                     new_combs.append([[*chosen_words, next_word], unique_letters])
                     checked_combs.add(unique_letters)
         combinations = new_combs
-        print(f"{len(combinations)} combinations of {tot_words+2} words starting with word \"{first_word}\"")
+        print(f"{len(combinations)} combinations of {tot_words+2} words starting with word \"{conv_word(first_word)}\"\n")
         if tot_words == 3:
             combins = [i[0] for i in combinations]
 
             if len(combins):
                 five_word_combinations += combins
-            print("Five word combinations found: ", *combins, sep='\n')
+                print("Five word combination found: ", *combins, sep='\n')
 
 
 for comb in five_word_combinations:
-    wrd = ', '.join(['|'.join(words_bitmap[word]) for word in comb])
+    wrd = ', '.join([conv_word(word) for word in comb])
     print(wrd)
 # print(five_word_combinations)
